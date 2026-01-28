f5ceed3eda40cd3ac87c8c0c60c9594379a728c6
zhmu <2376604595@qq.com>
2026-01-28 12:04:29 +0800

    Revert "change button text"
    This reverts commit 384801a07d39adc079a2eba1edd67830f498e8b6.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div style="padding: 24px">
-   <el-button type="primary">Hello Git + Element Plus 🚀</el-button>
-
+    <el-button type="primary">Hello Element Plus</el-button>
   </div>
 </template>
 
