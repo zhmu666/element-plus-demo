384801a07d39adc079a2eba1edd67830f498e8b6
zhmu <2376604595@qq.com>
2026-01-28 11:57:49 +0800

    change button text

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div style="padding: 24px">
-    <el-button type="primary">Hello Element Plus</el-button>
+   <el-button type="primary">Hello Git + Element Plus 🚀</el-button>
+
   </div>
 </template>
 
